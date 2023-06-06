8ff49698e4d2922587648334259e0828e17f6e33
fbansept <bansept.franck@gmail.com>
2023-06-06 09:53:50 +0200

    connexion

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.scss']
+  styleUrls: ['./connexion.component.scss'],
 })
 export class ConnexionComponent {
+  formulaire: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  onConnexion() {
+    if (this.formulaire.valid) {
+      this.http
+        .post('http://localhost:3000/login', this.formulaire.value)
+        .subscribe((reponse) => console.log(reponse));
+    }
+  }
 }
