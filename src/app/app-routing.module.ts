11035926599d31fabb79c261e09b40bc73bc9162
fbansept <bansept.franck@gmail.com>
2023-06-06 15:01:01 +0200

    ajout d'un guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { AccueilComponent } from './pages/accueil/accueil.component';
 import { ConnexionComponent } from './pages/connexion/connexion.component';
 import { Page404Component } from './pages/page404/page404.component';
 import { EditionArticleComponent } from './pages/edition-article/edition-article.component';
+import { authGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'connexion', component: ConnexionComponent },
-  { path: 'ajout-article', component: EditionArticleComponent },
+  {
+    path: 'ajout-article',
+    component: EditionArticleComponent,
+    canActivate: [authGuard],
+  },
   { path: 'modifier-article/:id', component: EditionArticleComponent },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: '**', component: Page404Component },
---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
+import { AuthentificationService } from './authentification.service';
+
+export const authGuard = () => {
+  const router = inject(Router);
+  const auth = inject(AuthentificationService);
+
+  //transforme l'obesvervable $jwt en UrlTree ou en booléen true
+  // si il renvoitg true il laisse passé l'utilisateur
+  // si il renvoit un UrlTree, il effectue une redirection
+  return auth.$jwt.pipe(
+    tap((jwt) => (jwt == null ? router.navigate(['/connexion']) : true))
+  );
+
+  //note :
+  //- tap permet d'effectuer une transformation sur la
+  //  valeur retournée par l'observable
+  //- pipe permet de résoudre directement un observable
+};
